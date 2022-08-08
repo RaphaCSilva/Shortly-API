94d6a553fd21fd77fe3de8ce4181f9daa432d310
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-08-08 00:42:57 -0300

    getUser feito não testado

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,32 @@
-export async function getUserById(req, res){
+import db from "../pg/db.js";
 
+export async function getUser(req, res){
+    
+    const { user } = res.locals;
+
+    try {
+        
+        const visitantsSoma = await db.query(`
+            SELECT SUM(urls."visitCount") FROM urls WHERE urls."userId" = $1
+        `, [user.id]);
+        const [visitantsTotal] = visitantsSoma.rows;
+
+        const buscaUrlsFromUser = await db.query(`
+            SELECT * FROM urls WHERE urls."userId" = $1
+        `, [user.id]);
+        const userUrls = buscaUrlsFromUser.rows;
+
+        res.send({
+            id: user.id,
+            name: user.name,
+            visitCount: visitantsTotal || 0,
+            shortenedUrls: userUrls
+        });
+
+    } catch (error) {
+       console.log(error);
+       return res.sendStatus(500); 
+    }
 }
 
 export async function getRanking(req, res){
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { validateToken } from "../middlewares/validateToken.js";
-import { getUserById, getRanking } from "../controllers/usersController.js"
+import { getUser, getRanking } from "../controllers/usersController.js"
 
 const userRouter = Router();
 
-userRouter.get("/users/:id", validateToken, getUserById);
+userRouter.get("/users/me", validateToken, getUser);
 userRouter.get("/ranking", getRanking);
 
 export default userRouter;
