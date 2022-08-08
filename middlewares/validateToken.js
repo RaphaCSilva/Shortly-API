6072669879bd6c933754e2713773ea4994278b37
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-08-07 23:01:38 -0300

    validação de token implementada

---
+++ middlewares/validateToken.js
@@ -0,0 +1,37 @@
+import db from "../pg/db.js"
+
+export async function validateToken(req, res, next){
+
+    const authorization = req.headers.authorization;
+    const token = authorization?.replace("Bearer", "");
+  
+    try {
+
+        if(!token){
+            return res.sendStatus(401);
+        }
+
+        const { rows: sessions } = await db.query(`SELECT * FROM sessions WHERE token = $1`, [token]);
+        const [session] = sessions;
+        if(!session){
+            return res.sendStatus(401);
+        }
+
+        const { rows: users } = await db.query(`SELECT * FROM users WHERE id = $1`, [session.userId]);
+        const [user] = users;
+        if(!user){
+            return res.sendStatus(401);
+        }
+
+        res.locals.user = user;
+        
+        next();
+
+    } catch (error) {
+        
+        console.log(error);
+        res.sendStatus(500);
+
+    }
+
+}
--- routes/urlsRouter.js
+++ routes/urlsRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { deleteUrl, getUrlById, openUrl, shortenUrl } from "../controllers/urlController.js";
+import { validateToken } from "../middlewares/validateToken.js";
 import { validateUrl } from "../middlewares/validateUrl.js";
 
 const urlsRouter = Router();
 
-urlsRouter.post("/urls/shorten", validateUrl, shortenUrl);
+urlsRouter.post("/urls/shorten", validateUrl, validateToken, shortenUrl);
 urlsRouter.get("/urls/:id", getUrlById)
 urlsRouter.get("/urls/open/:shortUrl", openUrl)
 urlsRouter.delete("urls/:id", deleteUrl)
