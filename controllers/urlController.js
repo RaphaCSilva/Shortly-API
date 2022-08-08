2316d9801a90c0762b0481f30d6ee7bc5227795b
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-08-07 23:12:44 -0300

    encurtando urls

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,7 +1,26 @@
 import { nanoid } from "nanoid";
+import db from "../pg/db.js";
 
 export async function shortenUrl(req, res){
+    
+    const { id } = res.locals.user;
+    const { url } = req.body
 
+    const charsUrl = 8;
+    const shortUrl = nanoid(charsUrl);
+
+    try {
+        await db.query(`
+            INSERT INTO urls(url, "userId", "shortUrl")
+            VALUES ($1, $2, $3)
+        `, [url, id, shortUrl]);
+        
+        res.status(201).send({shortUrl});
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
 
 }
 
